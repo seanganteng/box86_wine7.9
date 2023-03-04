5e8fb02120a1f9fbfbf8b0683d6b7a17eec7df35
smichaelf <48561347+seanganteng@users.noreply.github.com>
2023-03-04 20:21:56 +0700

    Update box86wine7.9.sh

--- box86wine7.9.sh
+++ box86wine7.9.sh
@@ -37,6 +37,7 @@ mkdir wine
 cd wine
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OG9W1CY_gvgrQx_50RSbYA-pTm8dGOSd' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1OG9W1CY_gvgrQx_50RSbYA-pTm8dGOSd" -O WINE-7.9.tar.gz && rm -rf /tmp/cookies.txt
+
 tar -xvf *.tar.gz
 
 echo "Do you want to install software render? (yes/no)"
